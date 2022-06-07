3f45bc60933965b161f93fe06f2b596db338ca3f
halston <halston.sellentin@usnr.com>
2022-06-07 15:47:26 -0700

    3d perspective working

--- src/frontend/ja.js
+++ src/frontend/ja.js
@@ -1,15 +1,15 @@
 import vss from "./vs.js";
 import fss from "./fs.js";
 import { createShader, createProgram, resizeCanvasToDisplaySize, sleep } from "./utils.js"
-import {identity, multiply, translate, scale, projection, rotationX} from "./math.js"
+import {identity, multiply, translate, scale, projection, rotationXY, rotationXZ} from "./math.js"
 
 const szFLOAT = 4;
 
 function logMatrix(m)
 {
-    for (let i = 0; i < 3; i++)
+    for (let i = 0; i < 4; i++)
     {
-        console.log(m[0 * 3 + i], m[1 * 3 + i], m[2 * 3 + i]);
+        console.log(m[0 * 4 + i], m[1 * 4 + i], m[2 * 4 + i], m[3 * 4 + i]);
     }
 }
 
@@ -40,23 +40,34 @@ async function main() {
 
     gl.bindBuffer(gl.ARRAY_BUFFER, buff);
 
-    let positionsAndColor = [
-        0, 0, 1, 255, 0, 0,
-        0, 100, 1, 255, 0, 0,
-        20, 100, 1, 255, 0, 0,
-        20, 20, 1, 0, 255, 0,
-        50, 0, 1, 0, 0, 255,
+    let cubeData = [
+        -.5, -.5, -.5,      255, 0, 0,
+         .5, -.5, -.5,      255, 0, 0,
+         .5,  .5, -.5,      255, 0, 0,
+        -.5,  .5, -.5,      255, 0, 0,
+
+        -.5, -.5,  .5,      0, 255, 0,
+         .5, -.5,  .5,      0, 255, 0,
+         .5,  .5,  .5,      0, 255, 0,
+        -.5,  .5,  .5,      0, 255, 0,
+
+
     ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positionsAndColor), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeData), gl.STATIC_DRAW);
 
     let elBuff = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elBuff);
 
     let elements = [
+        //FrontFace
         0, 1, 2,
         2, 3, 0,
-        0, 4, 3
+
+        //LeftFace
+        0, 3, 4,
+        3, 4, 7
     ]
+    
 
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(elements), gl.STATIC_DRAW);
 
@@ -74,7 +85,7 @@ async function main() {
 
     gl.enableVertexAttribArray(colorAttribLocation);
 
-    // binds currently bound array_buffer (positionBuffer) to this attribPointer
+    // binds currently bound array_buffer (positionBuffer) & ebo (elBuff) to this attribPointer
     gl.vertexAttribPointer(colorAttribLocation, // vertex attribute to modify
         3, // how many elements per attribute
         gl.FLOAT, // type of individual element
@@ -89,6 +100,8 @@ async function main() {
     /////
     /////
 
+    //gl.enable(gl.CULL_FACE);
+    gl.enable(gl.DEPTH_TEST);
 
     let i = 0;
     //render loops
@@ -98,21 +111,21 @@ async function main() {
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
         gl.clearColor(0, 0, 0, 0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         gl.useProgram(program);
 
-        let p = projection(gl.canvas.clientWidth, gl.canvas.clientHeight, identity());
+        let p = projection(3.14 / 2, gl.canvas.clientWidth / gl.canvas.clientHeight, 1, 30);
         let projLoc = gl.getUniformLocation(program, "projection");
-        gl.uniformMatrix3fv(projLoc, false, p);
+        gl.uniformMatrix4fv(projLoc, false, p);
 
-        let rotMat = rotationX(i+=.1, translate(50, 50, identity()));
+        let rotMat = rotationXZ(i+=.05, translate(0, 0, -3, identity()));
         let rotLoc = gl.getUniformLocation(program, "rotation");
-        gl.uniformMatrix3fv(rotLoc, false, rotMat);
+        gl.uniformMatrix4fv(rotLoc, false, rotMat);
 
         gl.bindVertexArray(vao);
 
-        gl.drawElements(gl.TRIANGLES, 9, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, 12, gl.UNSIGNED_SHORT, 0);
         await sleep(33);
     }
 }
--- src/frontend/vs.js
+++ src/frontend/vs.js
@@ -1,16 +1,16 @@
 const glsl = x => x;
 
 let vss = glsl`#version 300 es
-    in vec3 a_position;
+    in vec4 a_position;
     in vec3 a_color;
 
-    uniform mat3 projection;
-    uniform mat3 rotation;
+    uniform mat4 projection;
+    uniform mat4 rotation;
     out vec3 Color;
 
     void main() {
 
-        gl_Position = vec4(projection * rotation * a_position, 1.f);
+        gl_Position = projection * rotation * a_position;
         Color = a_color;
     }
 `;
